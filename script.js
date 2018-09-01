fef7a159bc085fe4d02409d63eaea29a1dd0e2ee
Ira <irina.kalmykova83@gmail.com>
2018-09-01 12:02:07 +0300

    header end start portfolio

---
+++ script.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+    'use strict';
+    function loadMap() {
+    var center = new google.maps.LatLng(47.846460,35.149270);
+        var marker = new google.maps.Marker({
+            position: center,
+            draggable:true,
+            icon:'img/favicon.png',
+            title:'Click to zoom',
+            animation: google.maps.Animation.BOUNCE
+        });
+        var mapOptions = {
+            center: center,
+            zoom:12,
+            panControl: true,
+            zoomControl: true,
+            zoomControlOptions: {
+                style: google.maps.ZoomControlStyle.SMALL,
+                position:google.maps.ControlPosition.LEFT_BOTTOM
+            },
+            mapTypeControl: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
+            scaleControl: true,
+            streetViewControl: true,
+            overviewMapControl: true,
+
+        };
+        var infowindow = new google.maps.InfoWindow({
+            content:"My town"
+        });
+
+        google.maps.event.addListener(marker,'click',function() {
+            infowindow.open(map,marker);
+        });
+        var map = new google.maps.Map(document.getElementById("map"),mapOptions);
+        marker.setMap(map);
+
+
+        // google.maps.event.addDomListener(window, 'load', loadMap);
+    }
+
+
+    loadMap();
+});
+
