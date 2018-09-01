414204ca4e6951cd02263b34d40b09fec96a1c0b
Ira <irina.kalmykova83@gmail.com>
2018-09-01 21:24:25 +0300

    isotop well

--- script.js
+++ script.js
@@ -25,8 +25,33 @@ $(document).ready(function(){
         marker.setMap(map);
 
     }
-
-
     loadMap();
+
+    //Isotop
+    $('.button-group button').click(function () {
+        $('.hidden').removeClass("hidden");
+    });
+    //переключение Табов
+
+    $('div .tabs').click(function () {
+        if (!$(this).hasClass('is-checked')) {
+            $('.tabs.is-checked').removeClass('is-checked');
+            $(this).addClass('is-checked');
+        }
+    });
+
+    var $isotopeGrid = $('.grid');
+
+    $isotopeGrid.isotope({
+        itemSelector: '.element-item',
+        percentPosition: true
+    });
+    $('.filters button').click(function () {
+        var filterValue = $(this).data('filter');
+        console.log(filterValue);
+        $isotopeGrid.isotope({
+            filter: filterValue
+        });
+    });
 });
 
